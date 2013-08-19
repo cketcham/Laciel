02355f18cc0bf5091c5b9e4f6aa39ac2482d318a
Cameron Ketcham <cketcham@gmail.com>
2013-08-18 18:34:25 -0700

    using audio recorder

--- Lasciel/src/main/java/org/cketcham/lasciel/AudioRecorder.java
+++ Lasciel/src/main/java/org/cketcham/lasciel/AudioRecorder.java
@@ -4,12 +4,16 @@ import android.app.Notification;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.IBinder;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
 
@@ -19,7 +23,6 @@ import java.util.Calendar;
 public class AudioRecorder extends Service {
     private static final String TAG = "AudioRecorder";
     private static boolean mRunning;
-    private MediaRecorder mRecorder;
 
     public AudioRecorder() {
     }
@@ -45,47 +48,14 @@ public class AudioRecorder extends Service {
         super.onDestroy();
         mRunning = false;
 
-        if (mRecorder != null) {
-            mRecorder.stop();
-            mRecorder.release();
-            mRecorder = null;
-        }
+        stopRecording();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         super.onStartCommand(intent, flags, startId);
-        initRecorder();
+        startRecording();
         return START_STICKY;
-    }
-
-    private void initRecorder() {
-        mRecorder = new MediaRecorder();
-        resetRecorder();
-        mRecorder.setOnInfoListener(new MediaRecorder.OnInfoListener() {
-
-            @Override
-            public void onInfo(MediaRecorder mr, int what, int extra) {
-                if (what == MediaRecorder.MEDIA_RECORDER_INFO_MAX_DURATION_REACHED) {
-                    mr.stop();
-                    mr.reset();
-                    resetRecorder();
-                    startNextRecording();
-                }
-            }
-        });
-
-        startNextRecording();
-    }
-
-    private static int encoder = 0;
-    private void resetRecorder() {
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        int e = encoder++%4 + 1;
-        mRecorder.setAudioEncoder(e);
-        mRecorder.setOutputFile(getAudioFile(e));
-        mRecorder.setMaxDuration(5 * 60 * 1000);
     }
 
     private String getAudioFile(int e) {
@@ -102,16 +72,87 @@ public class AudioRecorder extends Service {
         return new File(day, name + "-"+e+".mp4").toString();
     }
 
-    private void startNextRecording() {
+    private static final int RECORDER_SAMPLERATE = 8000;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord recorder = null;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
+
+    int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+    int BytesPerElement = 2; // 2 bytes in 16bit format
+
+    private void startRecording() {
+
+        recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+
+        recorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            public void run() {
+                writeAudioDataToFile();
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+    }
+
+    //convert short to byte
+    private byte[] short2byte(short[] sData) {
+        int shortArrsize = sData.length;
+        byte[] bytes = new byte[shortArrsize * 2];
+        for (int i = 0; i < shortArrsize; i++) {
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+            sData[i] = 0;
+        }
+        return bytes;
+
+    }
+
+    private void writeAudioDataToFile() {
+        // Write the output audio in byte
+
+        String filePath = "/sdcard/voice8K16bitmono.pcm";
+        short sData[] = new short[BufferElements2Rec];
+
+        FileOutputStream os = null;
         try {
-            mRecorder.prepare();
-            mRecorder.start();
-        } catch (IllegalStateException e) {
-            Log.e(TAG, "error starting next recording", e);
-            stopSelf();
+            os = new FileOutputStream(filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (isRecording) {
+            // gets the voice output from microphone to byte format
+
+            recorder.read(sData, 0, BufferElements2Rec);
+            System.out.println("Short wirting to file" + sData.toString());
+            try {
+                // // writes the data to file from buffer
+                // // stores the voice buffer
+                byte bData[] = short2byte(sData);
+                os.write(bData, 0, BufferElements2Rec * BytesPerElement);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            os.close();
         } catch (IOException e) {
-            Log.e(TAG, "error starting next recording", e);
-            stopSelf();
+            e.printStackTrace();
+        }
+    }
+
+    private void stopRecording() {
+        // stops the recording activity
+        if (null != recorder) {
+            isRecording = false;
+            recorder.stop();
+            recorder.release();
+            recorder = null;
+            recordingThread = null;
         }
     }
 }
