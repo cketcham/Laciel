641a29097c293da33af364663a900336d946a7bc
Cameron Ketcham <cketcham@gmail.com>
2013-08-21 14:56:37 -0700

    trying audio recorder

--- Lasciel/src/main/java/org/cketcham/lasciel/AudioRecorder.java
+++ Lasciel/src/main/java/org/cketcham/lasciel/AudioRecorder.java
@@ -6,13 +6,17 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioFormat;
 import android.media.AudioManager;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -23,7 +27,6 @@ import java.util.Date;
 public class AudioRecorder extends Service {
     private static final String TAG = "AudioRecorder";
     private static boolean mRunning;
-    private MediaRecorder mRecorder;
     private AudioManager am;
     private BroadcastReceiver bluetoothStateReceiver = new BroadcastReceiver() {
 
@@ -34,13 +37,11 @@ public class AudioRecorder extends Service {
 
             if (AudioManager.SCO_AUDIO_STATE_CONNECTED == state) {
                 if(mRecorder == null) {
-                    initRecorder();
+                    startRecording();
                 }
             } else if (AudioManager.SCO_AUDIO_STATE_DISCONNECTED == state) {
                 if (mRecorder != null) {
-                    mRecorder.stop();
-                    mRecorder.release();
-                    mRecorder = null;
+                    stopRecording();
                 }
             }
         }
@@ -71,9 +72,7 @@ public class AudioRecorder extends Service {
         mRunning = false;
 
         if (mRecorder != null) {
-            mRecorder.stop();
-            mRecorder.release();
-            mRecorder = null;
+            stopRecording();
         }
 
         am.stopBluetoothSco();
@@ -93,21 +92,87 @@ public class AudioRecorder extends Service {
         return START_STICKY;
     }
 
-    private void initRecorder() {
-        mRecorder = new MediaRecorder();
-        mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-        mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.HE_AAC);
-        mRecorder.setOutputFile(getAudioFile());
+    private static final int RECORDER_SAMPLERATE = 8000;
+    private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+    private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private AudioRecord mRecorder = null;
+    private Thread recordingThread = null;
+    private boolean isRecording = false;
+
+    int BufferElements2Rec = 1024; // want to play 2048 (2K) since 2 bytes we use only 1024
+    int BytesPerElement = 2; // 2 bytes in 16bit format
+
+    private void startRecording() {
+
+        mRecorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
+                RECORDER_SAMPLERATE, RECORDER_CHANNELS,
+                RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
+
+        mRecorder.startRecording();
+        isRecording = true;
+        recordingThread = new Thread(new Runnable() {
+            public void run() {
+                writeAudioDataToFile();
+            }
+        }, "AudioRecorder Thread");
+        recordingThread.start();
+    }
+
+    //convert short to byte
+    private byte[] short2byte(short[] sData) {
+        int shortArrsize = sData.length;
+        byte[] bytes = new byte[shortArrsize * 2];
+        for (int i = 0; i < shortArrsize; i++) {
+            bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+            bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+            sData[i] = 0;
+        }
+        return bytes;
+
+    }
+
+    private void writeAudioDataToFile() {
+        // Write the output audio in byte
+
+        String filePath = getAudioFile();
+        short sData[] = new short[BufferElements2Rec];
+
+        FileOutputStream os = null;
         try {
-            mRecorder.prepare();
-            mRecorder.start();
-        } catch (IllegalStateException e) {
-            Log.e(TAG, "error starting next recording", e);
-            stopSelf();
+            os = new FileOutputStream(filePath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        while (isRecording) {
+            // gets the voice output from microphone to byte format
+
+            mRecorder.read(sData, 0, BufferElements2Rec);
+            System.out.println("Short wirting to file" + sData.toString());
+            try {
+                // // writes the data to file from buffer
+                // // stores the voice buffer
+                byte bData[] = short2byte(sData);
+                os.write(bData, 0, BufferElements2Rec * BytesPerElement);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        try {
+            os.close();
         } catch (IOException e) {
-            Log.e(TAG, "error starting next recording", e);
-            stopSelf();
+            e.printStackTrace();
+        }
+    }
+
+    private void stopRecording() {
+        // stops the recording activity
+        if (null != mRecorder) {
+            isRecording = false;
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+            recordingThread = null;
         }
     }
 
